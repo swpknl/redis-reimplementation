fcc6b2e6eaa49d223ff2a4c9ea8372cbd5a45b1b
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-14 19:53:36 +0530

    7th stage

--- src/Impl/RedisCache.cs
+++ src/Impl/RedisCache.cs
@@ -12,20 +12,10 @@ public static class RedisCache
     {
         var array = request.Split("\r\n");
         var key = array[array.Length - 2];
-        Console.WriteLine(key);
         var isPresent = map.TryGetValue(key, out RedisCacheValue value);
-        Console.WriteLine(isPresent);
-        if (isPresent)
+        if (value.ExpiryDateTime >= DateTime.Now)
         {
-            if (value.ExpiryDateTime >= DateTime.Now)
-            {
-                return new KeyResponse(value.Value);    
-            }
-            else
-            {
-                return new NullResponse();
-            }
-                
+            return new KeyResponse(value.Value);    
         }
         else
         {
@@ -38,15 +28,10 @@ public static class RedisCache
         var array = request.Split("\r\n");
         if (request.ToLower().Contains("px"))
         {
-            Console.WriteLine("in Px");
             var key = array[array.Length - 6];
-            Console.WriteLine(key);
             var value = array[array.Length - 4];
-            Console.WriteLine(value);
             var timeout = double.Parse(array[array.Length - 2]);
-            Console.WriteLine(timeout);
             var datetime = DateTime.Now.AddMilliseconds(timeout);
-            
             map.TryAdd(key, new RedisCacheValue(value, datetime));
         }
         else
