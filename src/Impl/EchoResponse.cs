c3b246d1c16946f8df9575844052465058ab5752
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-14 17:44:25 +0530

    4th stage

--- src/Impl/EchoResponse.cs
+++ src/Impl/EchoResponse.cs
@@ -12,8 +12,8 @@ public class EchoResponse : IResponse
     
     public string GetResponse()
     {
-        var value = this.request.Split("\r\n");
-        return value[value.Length - 2];
-        return string.Empty;
+        var array = this.request.Split("\r\n");
+        var value = array[array.Length - 2];
+        return $"${value.Length}\r\n{value}\r\n";
     }
 }
