4c6f85ecdee8bd25883fe06d5f78072d6efc37fd
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-14 19:21:27 +0530

    6th stage

--- src/Server.cs
+++ src/Server.cs
@@ -6,4 +6,4 @@ using codecrafters_redis;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Starting server");
 var server = new RedisServer();
-server.Start();
+await server.Start();
