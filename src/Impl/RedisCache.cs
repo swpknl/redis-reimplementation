f535ca0a551e6c736396b64b04c4bc87c6ea0b3d
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-14 19:47:40 +0530

    7th stage

--- src/Impl/RedisCache.cs
+++ src/Impl/RedisCache.cs
@@ -39,10 +39,15 @@ public static class RedisCache
         var array = request.Split("\r\n");
         if (request.ToLower().Contains("px"))
         {
+            Console.WriteLine("in Px");
             var key = array[array.Length - 6];
+            Console.WriteLine(key);
             var value = array[array.Length - 4];
+            Console.WriteLine(value);
             var timeout = double.Parse(array[array.Length - 2]);
+            Console.WriteLine(timeout);
             var datetime = DateTime.Now.AddMilliseconds(timeout);
+            
             map.TryAdd(key, new RedisCacheValue(value, datetime));
         }
         else
